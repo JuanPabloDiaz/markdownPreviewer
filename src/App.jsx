2e5e25b3a3fe07e72b861cdd3c18353ec3278e02
Juan Diaz <juanchodis@hotmail.com>
2023-12-19 05:46:20 -0500

    [add] Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { marked } from "marked"; // Markdown parser library
 import "./App.css";
+import Footer from "./Components/Footer.jsx";
 
 // Enable GitHub flavored markdown (GFM)
 marked.setOptions({
@@ -272,14 +273,7 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="flex justify-end fixed sm:bottom-6 bottom-2 w-5/6 gap-2">
-          <p className="text-sm sm:text-md md:text-lg font-normal text-[#5f5f5f]">
-            Developed by:
-          </p>
-          <span className="text-sm sm:text-md md:text-lg font-normal text-[#999999] hover:text-[#F8F9FA] transition duration-300">
-            <a href="https://jpdiaz.dev/">Juan Diaz</a>
-          </span>
-        </div>
+        <Footer />
       </div>
     </>
   );
